ea97d8d3de3f12da0ecbcd5349a8bac9f750b239
cws09147 <cws09147@ly.com>
2015-01-17 19:21:58 +0800

    add code

--- src/com/sport365/badminton/activity/MainActivity.java
+++ src/com/sport365/badminton/activity/MainActivity.java
@@ -1,7 +1,5 @@
 package com.sport365.badminton.activity;
 
-import java.util.ArrayList;
-
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.view.KeyEvent;
@@ -18,6 +16,7 @@ import com.sport365.badminton.activity.fragment.HomeMyFragment;
 import com.sport365.badminton.activity.fragment.HomePageFragment;
 import com.sport365.badminton.activity.fragment.HomePayFragment;
 import com.sport365.badminton.utils.ULog;
+import com.sport365.badminton.utils.Utilities;
 
 /**
  * 首页的4个fragment
@@ -25,39 +24,38 @@ import com.sport365.badminton.utils.ULog;
  */
 public class MainActivity extends BaseActivity implements OnCheckedChangeListener {
 
-	private RadioGroup				rg_menu;
-	private RadioButton				rb_menu_mian;
-	private RadioButton				rb_menu_pay;
-	private RadioButton				rb_menu_ball_friend;
-	private RadioButton				rb_menu_my;
+	/** 再按一次退出应用 */
+	private long			exitTime	= 0;
 
-	private ArrayList<BaseFragment>	fragments	= new ArrayList<BaseFragment>();	// 存放fragment
-	private int						currentTab	= 0;								// 当前Tab页面索引
+	private RadioGroup		rg_menu;
+	private RadioButton		rb_menu_mian;
+	private RadioButton		rb_menu_pay;
+	private RadioButton		rb_menu_ball_friend;
+	private RadioButton		rb_menu_my;
 
 	/** 当前显示的fragment */
-	private BaseFragment			mCurrentFragment;
+	private BaseFragment	mCurrentFragment;
 	/** 首页fragment */
-	private BaseFragment			mHomeFragment;
+	private BaseFragment	mHomeFragment;
 	/** 充值fragment */
-	private BaseFragment			mPayFragment;
+	private BaseFragment	mPayFragment;
 	/** 惠球友fragment */
-	private BaseFragment			mBallfriendFragment;
+	private BaseFragment	mBallfriendFragment;
 	/** 我的fragment */
-	private BaseFragment			mMyFragment;
+	private BaseFragment	mMyFragment;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		initActionBar();
 		mActionbar_left.setImageResource(R.drawable.icon_title365_logo);
 		mActionbar_title.setVisibility(View.GONE);
-		initMainView();
+		findViews();
 		rb_menu_mian.setChecked(true);
 		ULog.debug("--->onCreate");
 	}
 
-	private void initMainView() {
+	private void findViews() {
 		rg_menu = (RadioGroup) findViewById(R.id.rg_menu);
 		rb_menu_mian = (RadioButton) findViewById(R.id.rb_menu_mian);
 		rb_menu_pay = (RadioButton) findViewById(R.id.rb_menu_pay);
@@ -70,6 +68,7 @@ public class MainActivity extends BaseActivity implements OnCheckedChangeListene
 	public void onCheckedChanged(RadioGroup group, int checkedId) {
 		FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
 		if (mCurrentFragment != null) {
+			mCurrentFragment.onPause();
 			fragmentTransaction.hide(mCurrentFragment);
 		}
 
@@ -85,7 +84,7 @@ public class MainActivity extends BaseActivity implements OnCheckedChangeListene
 			mCurrentFragment = mHomeFragment;
 			break;
 		case R.id.rb_menu_pay:
-			if (mPayFragment == null) {
+			if (mPayFragment != null) {
 				fragmentTransaction.show(mPayFragment);
 			}
 			else {
@@ -127,7 +126,19 @@ public class MainActivity extends BaseActivity implements OnCheckedChangeListene
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		switch (keyCode) {
 		case KeyEvent.KEYCODE_BACK:
-
+			if (rg_menu.getCheckedRadioButtonId() != R.id.rb_menu_mian) {
+				rg_menu.check(R.id.rb_menu_mian);
+			}
+			else {
+				if ((System.currentTimeMillis() - exitTime) > 2000) {
+					Utilities.showToast(mContext.getResources().getString(R.string.press_more_exit), this);
+					exitTime = System.currentTimeMillis();
+				}
+				else {
+					MainActivity.this.finish();
+				}
+				return true;
+			}
 			break;
 		default:
 			break;
