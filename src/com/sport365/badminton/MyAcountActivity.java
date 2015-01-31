d67b1cb3e03785453d76d284a1f6a3cbda87e501
VincentChan <wansong5566@126.com>
2015-01-31 14:37:46 +0800

    提交代码

---
+++ src/com/sport365/badminton/MyAcountActivity.java
@@ -0,0 +1,17 @@
+package com.sport365.badminton;
+
+import android.os.Bundle;
+
+/**
+ * Created by vincent on 15/1/31.
+ */
+public class MyAcountActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setActionBarTitle("我的账户");
+        setContentView(R.layout.activity_myacount);
+
+    }
+}
--- src/com/sport365/badminton/activity/fragment/HomePayFragment.java
+++ src/com/sport365/badminton/activity/fragment/HomePayFragment.java
@@ -4,27 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-
 import com.sport365.badminton.BaseFragment;
 import com.sport365.badminton.R;
-import com.sport365.badminton.view.DialogFactory;
 
 public class HomePayFragment extends BaseFragment {
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.home_pay_layout, container, false);
-		Button btn_btn = (Button) view.findViewById(R.id.btn_btn);
-
-		btn_btn.setOnClickListener(new Button.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				new DialogFactory(getActivity()).showDialog3Btn("提示", "这是内容", "左边", "中间", "右边", null, true);
-			}
-		});
-
-		return view;
-	}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.home_pay_layout, container, false);
+        return view;
+    }
 }
