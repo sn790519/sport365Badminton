9002a369db7f50293389ad8bae9d6b9e72a65586
VincentChan <wansong5566@126.com>
2015-03-15 16:56:25 +0800

    ---

--- src/com/sport365/badminton/activity/fragment/HomeMyFragment.java
+++ src/com/sport365/badminton/activity/fragment/HomeMyFragment.java
@@ -19,102 +19,107 @@ import com.sport365.badminton.utils.Utilities;
 public class HomeMyFragment extends BaseFragment implements View.OnClickListener {
 
 
-	/**
-	 * 顶部登陆
-	 */
-	private RelativeLayout rl_top;
-	private RelativeLayout rl_user_account;
-	/**
-	 * 在线充值
-	 */
-	private RelativeLayout rl_pay;
-	/**
-	 * 充值记录
-	 */
-	private RelativeLayout rl_pay_history;
-	private TextView tv_no_login;
-	private TextView tv_user_name;
-	private TextView tv_user_score;
-	private Button btn_logout;
+    /**
+     * 顶部登陆
+     */
+    private RelativeLayout rl_top;
+    private RelativeLayout rl_user_account;
+    /**
+     * 在线充值
+     */
+    private RelativeLayout rl_pay;
+    /**
+     * 充值记录
+     */
+    private RelativeLayout rl_pay_history;
+    private TextView tv_no_login;
+    private TextView tv_user_name;
+    private TextView tv_user_score;
+    private Button btn_logout;
 
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.home_my_layout, container, false);
-		findViews(view);
-		return view;
-	}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.home_my_layout, container, false);
+        findViews(view);
+        return view;
+    }
 
-	private void findViews(View view) {
-		btn_logout = (Button) view.findViewById(R.id.btn_logout);
-		btn_logout.setOnClickListener(this);
-		rl_pay = (RelativeLayout) view.findViewById(R.id.rl_pay);
-		rl_pay.setOnClickListener(this);
-		rl_pay_history = (RelativeLayout) view.findViewById(R.id.rl_pay_history);
-		rl_pay_history.setOnClickListener(this);
-		rl_top = (RelativeLayout) view.findViewById(R.id.rl_top);
-		rl_top.setOnClickListener(this);
-		rl_user_account = (RelativeLayout) view.findViewById(R.id.rl_user_account);
-		tv_no_login = (TextView) view.findViewById(R.id.tv_no_login);
-		tv_user_name = (TextView) view.findViewById(R.id.tv_user_name);
-		tv_user_score = (TextView) view.findViewById(R.id.tv_user_score);
-		if (SystemConfig.isLogin()) {
-			tv_no_login.setVisibility(View.GONE);
-			rl_user_account.setVisibility(View.VISIBLE);
-		} else {
-			tv_no_login.setVisibility(View.VISIBLE);
-			rl_user_account.setVisibility(View.GONE);
-		}
-	}
+    private void findViews(View view) {
+        btn_logout = (Button) view.findViewById(R.id.btn_logout);
+        btn_logout.setOnClickListener(this);
+        if (null != SystemConfig.loginResBody && SystemConfig.isLogin()) {
+            btn_logout.setVisibility(View.VISIBLE);
+        } else {
+            btn_logout.setVisibility(View.GONE);
+        }
+        rl_pay = (RelativeLayout) view.findViewById(R.id.rl_pay);
+        rl_pay.setOnClickListener(this);
+        rl_pay_history = (RelativeLayout) view.findViewById(R.id.rl_pay_history);
+        rl_pay_history.setOnClickListener(this);
+        rl_top = (RelativeLayout) view.findViewById(R.id.rl_top);
+        rl_top.setOnClickListener(this);
+        rl_user_account = (RelativeLayout) view.findViewById(R.id.rl_user_account);
+        tv_no_login = (TextView) view.findViewById(R.id.tv_no_login);
+        tv_user_name = (TextView) view.findViewById(R.id.tv_user_name);
+        tv_user_score = (TextView) view.findViewById(R.id.tv_user_score);
+        if (SystemConfig.isLogin()) {
+            tv_no_login.setVisibility(View.GONE);
+            rl_user_account.setVisibility(View.VISIBLE);
+        } else {
+            tv_no_login.setVisibility(View.VISIBLE);
+            rl_user_account.setVisibility(View.GONE);
+        }
+    }
 
-	@Override
-	public void onClick(View v) {
-		super.onClick(v);
-		switch (v.getId()) {
-			case R.id.btn_logout:
-				SystemConfig.clearData();
-				if (null != SystemConfig.loginResBody && SystemConfig.isLogin()) {
-					tv_no_login.setVisibility(View.GONE);
-					rl_user_account.setVisibility(View.VISIBLE);
-					tv_user_name.setText(SystemConfig.loginResBody.account);
-					tv_user_score.setText(SystemConfig.loginResBody.consumeMoney);
-				} else {
-					tv_no_login.setVisibility(View.VISIBLE);
-					rl_user_account.setVisibility(View.GONE);
-					tv_user_name.setText("");
-					tv_user_score.setText("");
-				}
-				Utilities.showToast(getString(R.string.logout_success), getActivity());
-				break;
-			case R.id.rl_pay:
-				((MainActivity) getActivity()).rb_menu_pay.setChecked(true);
-				break;
-			case R.id.rl_pay_history:
-				break;
-			case R.id.rl_top:
-				if (SystemConfig.isLogin()) {
-					startActivity(new Intent(getActivity(), MyAccountActivity.class));
-				} else {
-					startActivity(new Intent(getActivity(), LoginActivity.class));
-				}
-				break;
-		}
-	}
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+        switch (v.getId()) {
+            case R.id.btn_logout:
+                SystemConfig.clearData();
+                if (null != SystemConfig.loginResBody && SystemConfig.isLogin()) {
+                    tv_no_login.setVisibility(View.GONE);
+                    rl_user_account.setVisibility(View.VISIBLE);
+                    tv_user_name.setText(SystemConfig.loginResBody.account);
+                    tv_user_score.setText(SystemConfig.loginResBody.consumeMoney);
+                } else {
+                    tv_no_login.setVisibility(View.VISIBLE);
+                    rl_user_account.setVisibility(View.GONE);
+                    tv_user_name.setText("");
+                    tv_user_score.setText("");
+                }
+                Utilities.showToast(getString(R.string.logout_success), getActivity());
+                break;
+            case R.id.rl_pay:
+                ((MainActivity) getActivity()).rb_menu_pay.setChecked(true);
+                break;
+            case R.id.rl_pay_history:
+                break;
+            case R.id.rl_top:
+                if (SystemConfig.isLogin()) {
+                    startActivity(new Intent(getActivity(), MyAccountActivity.class));
+                } else {
+                    startActivity(new Intent(getActivity(), LoginActivity.class));
+                }
+                break;
+        }
+    }
 
-	@Override
-	public void onResume() {
-		super.onResume();
-		if (null != SystemConfig.loginResBody && SystemConfig.isLogin()) {
-			tv_no_login.setVisibility(View.GONE);
-			rl_user_account.setVisibility(View.VISIBLE);
-			tv_user_name.setText(SystemConfig.loginResBody.account);
-			tv_user_score.setText(SystemConfig.loginResBody.consumeMoney);
-		} else {
-			tv_no_login.setVisibility(View.VISIBLE);
-			rl_user_account.setVisibility(View.GONE);
-			tv_user_name.setText("");
-			tv_user_score.setText("");
-		}
-	}
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (null != SystemConfig.loginResBody && SystemConfig.isLogin()) {
+            tv_no_login.setVisibility(View.GONE);
+            rl_user_account.setVisibility(View.VISIBLE);
+            tv_user_name.setText(SystemConfig.loginResBody.account);
+            tv_user_score.setText(SystemConfig.loginResBody.consumeMoney);
+        } else {
+            tv_no_login.setVisibility(View.VISIBLE);
+            rl_user_account.setVisibility(View.GONE);
+            tv_user_name.setText("");
+            tv_user_score.setText("");
+        }
+    }
 
 }
