c32f6d16958916feb1f166e50702b72b9a92d901
VincentChan <wansong5566@126.com>
2015-04-14 00:18:13 +0800

    js

--- src/com/sport365/badminton/utils/LoginJavaScript.java
+++ src/com/sport365/badminton/utils/LoginJavaScript.java
@@ -1,24 +1,32 @@
 package com.sport365.badminton.utils;
 
 import android.content.Intent;
-import android.webkit.WebView;
+import android.os.Handler;
+import android.webkit.JavascriptInterface;
 import com.sport365.badminton.activity.BallFriendActivity;
 import com.sport365.badminton.activity.LoginActivity;
 
 public class LoginJavaScript {
 
 
-	private BallFriendActivity activity;
-	private WebView webView;
+    private BallFriendActivity activity;
+    private Handler myHandler;
 
-	public LoginJavaScript(BallFriendActivity activity) {
+    public LoginJavaScript(BallFriendActivity activity, Handler myHandler) {
+        this.myHandler = myHandler;
+        this.activity = activity;
+    }
 
-		this.activity = activity;
-	}
+    @JavascriptInterface
+    public void startLoginActivity() {
+        myHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(activity, LoginActivity.class);
+                activity.startActivityForResult(intent,0);
+            }
+        });
 
-	public void startLoginActivity() {
-		Intent intent = new Intent(activity, LoginActivity.class);
-		activity.startActivity(intent);
-	}
+    }
 }
 
